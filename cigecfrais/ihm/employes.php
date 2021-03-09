d168be03cb661e08d25c484bc3e2d09a7be6c397
Chultos <65951441+Chultos@users.noreply.github.com>
2021-03-09 11:11:50 +0100

    Version 0.2
    Ajout d'un tableau pour la page des employés
    Ajout d'une barre de recherche pour faciliter la recherche des employés dans le tableau

--- cigecfrais/ihm/employes.php
+++ cigecfrais/ihm/employes.php
@@ -3,7 +3,29 @@
 ?>
 
 <body>
-    <p>Liste des employés de l'entreprise</p> 
+    <script src="scripts/searchbar.js"></script>
+
+    <h1>Liste des employés de l'entreprise <br><br><br></h1>
+    <input type="text" id="searchInput" onkeyup="trierTable()" placeholder="Rechercher un employé...">
+    <table id="tableEmployes" class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">NOM Prénom</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                // Création des lignes pour chaque employé
+                foreach($les_employes as $un_employe) {
+                    echo "<tr>";
+                    echo "<th scope=\"row\">".$un_employe["id"]."</th>";
+                    echo "<td>".$un_employe["nom"]." ".$un_employe["prenom"]."</td>";
+                    echo "</tr>";
+                }
+            ?>
+        </tbody>
+    </table>
 </body>
 
 <?php 
